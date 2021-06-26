4296fb98c8e11e8074894227ee6eef68eeb559f1
oteniel-santos <tynelsantos@gmail.com>
2021-06-26 12:13:54 -0400

    Config do Banco de Dados - SQlite

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,6 @@
-
+import "reflect-metadata";
 import express from 'express';
 const app = express();
-app.get('/teste', (request, response) => {
-  return response.send('Olá NLW');
-});
-
-app.post('/teste-post', (request, response) => {
-  return response.send('Olá NLW - POST');
-});
+import "./database"
 
 app.listen(3005, () => console.log('Server is running'));
